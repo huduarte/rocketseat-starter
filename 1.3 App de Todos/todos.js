ce885e013de74c396bc5130476475ae2c2d8b349
Hudson Duarte <hudsoneeto@outlook.com>
2020-07-22 20:55:32 -0300

    App de Todos

--- 1.3 App de Todos/todos.js
+++ 1.3 App de Todos/todos.js
@@ -2,26 +2,27 @@ var listElement = document.querySelector('#app ul')
 var inputElement = document.querySelector('#app input')
 var btnElement = document.querySelector('#app button')
 
-
+//Guardando todo no localstorage
 var todos = JSON.parse(localStorage.getItem('list_todos')) || []
 
 function renderTodos(){
     listElement.innerHTML = ' ';
     for (todo of todos){
+        //Criar texto
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo)
 
+        //Botao excluir
         var linkElement = document.createElement('a')
-
         linkElement.setAttribute('href', '#')
-
-        var pos = todos.indexOf(todo);
-        linkElement.setAttribute('onclick', 'deleteTodo(' +pos+ ')')
-
         var linkText = document.createTextNode('Excluir')
         linkElement.appendChild(linkText)
 
+        //Apagar todo
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' +pos+ ')')
 
+        
         todoElement.appendChild(todoText);
         todoElement.appendChild(linkElement)
         listElement.appendChild(todoElement)
@@ -32,7 +33,7 @@ renderTodos()
 
 function addTodo(){
     var todoText = inputElement.value;
-
+    //Adicionar um novo item em um array
     todos.push(todoText);
     inputElement.value = ''
     renderTodos()
@@ -42,6 +43,7 @@ function addTodo(){
 btnElement.onclick = addTodo;
 
 function deleteTodo(pos){
+    //Apagar um item de um array
     todos.splice(pos, 1);
     renderTodos();
     saveToStorage()
@@ -49,5 +51,6 @@ function deleteTodo(pos){
 
 
 function saveToStorage(){
+    //JSON transforma um array em uma lista de string
     localStorage.setItem('list_todos', JSON.stringify(todos))
 }
