7503ba5f46b399baa4e2ab8b42a510ce34609b55
Hudson Duarte <hudsoneeto@outlook.com>
2020-07-29 18:51:46 -0300

    Aplicação com ES6+

--- Curso ES6/webpack.config.js
+++ Curso ES6/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: ['@babel/polyfill','./src/DesafiosAsync/main.js',],
+    entry: ['@babel/polyfill','./src/AppES6+/main.js',],
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
